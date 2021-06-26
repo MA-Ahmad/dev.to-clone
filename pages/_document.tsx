b11de9eff7cb316b75c7af064af2c51a17a3ecb8
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-26 05:13:20 -0700

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+          <meta property="og:title" content="DEV Community 👨‍💻👨‍💻" />
+          <meta property="og:image" content="/devto.png" />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
