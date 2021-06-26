6bb8288dfdb135cae390735f4d55fb5439306a6b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-26 05:13:05 -0700

    Create meta.tsx

---
+++ components/layout/meta.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Devto",
+  keywords: "Nextjs, Charkra-UI, React",
+  description: "Dev.to clone build by Muhammad Ahmad"
+};
+
+export default Meta;
