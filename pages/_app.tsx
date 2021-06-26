d4bc8a57fab1fa1a97caf9204ecc49743b85db39
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-26 07:14:18 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
