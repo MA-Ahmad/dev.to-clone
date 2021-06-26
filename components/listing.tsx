622fe69f9067d23d548d04bad9a1a90b95c8db00
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-26 07:14:15 -0700

    Update listing.tsx

--- components/listing.tsx
+++ components/listing.tsx
@@ -5,8 +5,11 @@ import {
   Heading,
   Text,
   Spacer,
-  VStack
+  VStack,
+  Skeleton
 } from "@chakra-ui/react";
+import useSWR from "swr";
+import { useState } from "react";
 
 function ListHeading() {
   return (
@@ -30,56 +33,84 @@ function HashTagHeading() {
   );
 }
 
-const List = ({ headingType }) => {
+const fetcher = url => fetch(url).then(res => res.json());
+
+const List = () => {
+  const { data, error } = useSWR("https://dev.to/api/listings", fetcher);
+
+  if (error) return <Box>failed to load</Box>;
+  if (!data)
+    return (
+      <Box as="section" bg="white" borderRadius="md" border="1px solid #E2E4E6" width="100%">
+        <ListHeading />
+        {[1, 2, 3, 4, 5].map(id => {
+          return (
+            <Box borderBottom="1px solid #E2E4E6" width="100%" p="3">
+              <Skeleton height="15vh" borderRadius="5px" width="100%" />
+            </Box>
+          );
+        })}
+      </Box>
+    );
+
   return (
     <Box as="section" bg="white" borderRadius="md" border="1px solid #E2E4E6">
-      {headingType === "hashtag" ? <HashTagHeading /> : <ListHeading />}
-      <Box borderBottom="1px solid #E2E4E6">
-        <Box p="3">
-          <Text>Javascript developer (~ 2 YOE) looking for remote work</Text>
-          <Text mt="2" color="#4d5760" fontSize="14px">
-            forhire
-          </Text>
-        </Box>
-      </Box>
-      <Box borderBottom="1px solid #E2E4E6">
-        <Box p="3">
-          <Text>
-            Building Progressive Web Apps - Hands-on Workshop - 4/29 1pm ET (GMT
-            -4)
-          </Text>
-          <Text mt="2" color="#4d5760" fontSize="14px">
-            events
-          </Text>
-        </Box>
-      </Box>
-      <Box borderBottom="1px solid #E2E4E6">
-        <Box p="3">
-          <Text>
-            Want to know how a JavaScript framework works under the hood?
-          </Text>
-          <Text mt="2" color="#4d5760" fontSize="14px">
-            education
-          </Text>
-        </Box>
-      </Box>
-      <Box>
-        <Box p="3">
-          <Text>Pair Programming with Jhey</Text>
-          <Text mt="2" color="#4d5760" fontSize="14px">
-            events
-          </Text>
-        </Box>
-      </Box>
+      <ListHeading />
+      {data.slice(0, 7).map(list => (
+        <ListBox title={list.title} category={list.category} slug={list.slug} />
+      ))}
     </Box>
   );
-}
+};
 
-const Listing = (props) => {
+const ListBox = ({ title, category, slug }) => {
+  return (
+    <Link
+      href={`https://dev.to/listings/${category}/${slug}`}
+      _hover={{ textDecoration: "none", color: "blue" }}
+      isExternal
+    >
+      <Box borderBottom="1px solid #E2E4E6" _hover={{ bg: "gray.50" }}>
+        <Box p="3">
+          <Text>{title}</Text>
+          <Text mt="2" color="#4d5760" fontSize="14px">
+            {category}
+          </Text>
+        </Box>
+      </Box>
+    </Link>
+  );
+};
+
+const News = () => {
+  return (
+    <Box as="section" bg="white" borderRadius="md" border="1px solid #E2E4E6">
+      <HashTagHeading />
+      {[
+        {
+          title: "Javascript developer (~ 2 YOE) looking for remote work",
+          category: "events",
+          slug: ""
+        },
+        {
+          title:
+            "Want to know how a JavaScript framework works under the hood?",
+          category: "education",
+          slug: ""
+        },
+        { title: "Pair Programming with Jhey", category: "events", slug: "" }
+      ].map(news => (
+        <ListBox title={news.title} category={news.category} />
+      ))}
+    </Box>
+  );
+};
+
+const Listing = props => {
   return (
     <VStack as="aside" spacing="4" {...props}>
-      <List headingType="link" />
-      <List headingType="hashtag" />
+      <List />
+      <News />
     </VStack>
   );
 };
