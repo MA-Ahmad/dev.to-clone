cfb1152d6200d353e4be1cb6685c5d29057cf775
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-26 05:13:00 -0700

    Create skeleton.tsx

---
+++ components/feed/skeleton.tsx
@@ -0,0 +1,55 @@
+import { Box, HStack, Skeleton, Stack } from "@chakra-ui/react";
+
+const SkeletonCards = () => {
+  return (
+    <Stack spacing={3}>
+      {[1, 2, 3, 4, 5, 6, 7, 8].map(id => {
+        return (
+          <Box
+            key={id}
+            cursor="pointer"
+            borderWidth="1px"
+            shadow="md"
+            bg="#fbfdff"
+            position="relative"
+            rounded="md"
+            borderRadius="5px"
+          >
+            {id === 1 ? (
+              <Skeleton height="28vh" borderRadius="5px 5px 0 0" width="100%" />
+            ) : (
+              ""
+            )}
+            <Stack isInline justifyContent="space-between" mt={2} p={5}>
+              <Box width="100%" height="15vh">
+                <Stack isInline align="center" marginBottom="5px">
+                  <Box>
+                    <Skeleton
+                      size="sm"
+                      width="2em"
+                      height="2em"
+                      borderRadius="50%"
+                    />
+                  </Box>
+                  <Skeleton height="14px" width="20%" />
+                </Stack>
+                <Box pl="2.5em">
+                  <Skeleton height="16px" width="100%" />
+                  <Stack spacing={2} mt={1} isInline alignItems="center">
+                    <Skeleton height="14px" width="80%" />
+                  </Stack>
+                  <HStack spacing={2} mt={8} isInline alignItems="center">
+                    <Skeleton height="12px" width="20%" rounded="full" my={1} />
+                    <Skeleton height="12px" width="20%" rounded="full" my={1} />
+                  </HStack>
+                </Box>
+              </Box>
+            </Stack>
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default SkeletonCards;
